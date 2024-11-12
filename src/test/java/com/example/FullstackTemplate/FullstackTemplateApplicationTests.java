697edf3555917091560b2f5cfd398364b406e6b2
Irish Manalang <irish.r.manalang.mil@army.mil>
2024-11-12 15:10:39 -0600

    finished backend setup

---
+++ src/test/java/com/example/FullstackTemplate/FullstackTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FullstackTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
